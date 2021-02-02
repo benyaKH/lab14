139c0b7f39f7de30c82bffcd56160428b2df68eb
Kaofang <benyapa_kh@cmu.ac.th>
2021-02-02 17:30:18 +0700

    c4

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <cstdlib>
 #include <ctime>
 using namespace std;
 
@@ -16,4 +14,16 @@ int main(){
 	}
 	
 	return 0;
+}
+
+void shuffle(int &a,int &b,int &c,int &d){
+    int x[4]={a,b,c,d};
+    int y[4]={0,0,0,0} ;
+    do{
+        for(int i = 0 ;i<4;i++) y[i]=x[rand()%4] ;
+    }while(y[0]==y[1]||y[0]==y[2]||y[0]==y[3]||y[1]==y[2]||y[1]==y[3]||y[2]==y[3]);
+    a=y[0] ;
+    b=y[1] ;
+    c=y[2] ;
+    d=y[3] ;
 }
